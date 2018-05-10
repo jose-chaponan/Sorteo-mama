a64e30fcedb663f08894c91a89c4692bc885fa2c
José Iván <ivan@navike21.com>
2018-05-10 16:54:55 -0500

    subiendo archivos

--- data/conexion.php
+++ data/conexion.php
@@ -1,4 +1,4 @@
 <?php
-    $PDO = new PDO( 'mysql:host=localhost;dbname=registroslarcomar;charset=UTF8','root','' );
-    //$PDO = new PDO( 'mysql:host=localhost;dbname=adretail_registros_larcomar;charset=UTF8','adretail_ivan','Server2003' );
+    // $PDO = new PDO( 'mysql:host=localhost;dbname=registroslarcomar;charset=UTF8','root','' );
+    $PDO = new PDO( 'mysql:host=localhost;dbname=adretail_registros_larcomar;charset=UTF8','adretail_ivan','Server2003' );
 ?>
