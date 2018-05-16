68d2a4595e691f8bc4bb141df25c928f230348cb
José Iván <ivan@navike21.com>
2018-05-16 14:24:01 -0500

    agregar campo de telefono y fecha de registro

--- data/registro.php
+++ data/registro.php
@@ -10,13 +10,15 @@
     $txtemail       = $_POST["email"];
     $txtdistrito    = $_POST["distrito"];
     $selsexo        = $_POST["sexo"];
-
+    $telefono       = $_POST["telefono"];
 
     $sqlexiste = $PDO -> prepare("SELECT nombres, apellidos FROM sorteo_mayo_2018 WHERE nombres = :nombrese AND apellidos = :apellidose");
     $sqlexiste->bindParam(":nombrese", $txtnombres);
     $sqlexiste->bindParam(":apellidose", $txtapellidos);
     $sqlexiste->execute();
     $row_existe = $sqlexiste->rowCount();
+    
+    $fecha_registro = date("Y-m-d H:i:s");
 
     if ($row_existe != 0) {
         echo "existes";
@@ -27,18 +29,21 @@
         $sqlexisteEmail->execute();
         $row_existeEmail = $sqlexisteEmail->rowCount();
 
+
         if($row_existeEmail != 0){
             echo "emailexiste";
         }
         else{
-            $sql = $PDO -> prepare("INSERT INTO sorteo_mayo_2018( nombres, apellidos, fecha_nac, email, distrito, sexo ) VALUES ( :nombres, :apellidos, :fecha_nac, :email, :distrito, :sexo )");
+            $sql = $PDO -> prepare("INSERT INTO sorteo_mayo_2018( nombres, apellidos, fecha_nac, email, telefono, distrito, sexo, fecha_registro ) VALUES ( :nombres, :apellidos, :fecha_nac, :email, :telefono, :distrito, :sexo, :fecha_registro)");
 
             $sql->bindParam(":nombres", $txtnombres);
             $sql->bindParam(":apellidos", $txtapellidos);
             $sql->bindParam(":fecha_nac", $txtfecha_nac);
             $sql->bindParam(":email", $txtemail);
+            $sql->bindParam(":telefono", $telefono);
             $sql->bindParam(":distrito", $txtdistrito);
             $sql->bindParam(":sexo", $selsexo);
+            $sql->bindParam(":fecha_registro", $fecha_registro);
             // $sql->bindParam(":respuesta", $txtrespuesta);
             $sql->execute();
 
